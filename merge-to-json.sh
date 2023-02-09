4bb71d0fb53ae344145158500702108c4afb60bb
Jayant.Tang <jayant97@foxmail.com>
2023-02-10 00:22:11 +0800

    aaa4

--- merge-to-json.sh
+++ merge-to-json.sh
@@ -4,11 +4,8 @@ input_file=$1
 
 cat "$input_file" | while read line; do
   user=$(echo $line | sed 's#https://github.com/\(.*\)/\(.*\)#\1#')
-  repo=$(echo $line | sed 's#https://github.com/\(.*\)/\(.*\)#\2#') | sed 's/\.git$//'
+  repo=$(echo $line | sed 's#https://github.com/\(.*\)/\(.*\)#\2#' | sed 's#[.]git$##') 
   echo "{\"src\":\"$user\",\"repo\":\"$repo\"}"
-done > ~/merge.json
-
-##
-#  | \
-# jq -s add | \
-# jq -c 'reduce .[] as $i ({}; .[$i.src]= ($i + .[$i.src] |= {"repo": $i.repo})) | to_entries[] | {src: .key, repo: .value.repo}'
+done | \
+jq -s add | \
+jq -c 'reduce .[] as $i ({}; .[$i.src]= ($i + .[$i.src] |= {"repo": $i.repo})) | to_entries[] | {src: .key, repo: .value.repo}' > ~/merge.json 
