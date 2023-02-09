99b3bccb2dbf5479f5e7826b8e1a76357d3a1ed3
Jayant.Tang <jayant97@foxmail.com>
2023-02-09 22:21:21 +0800

    chat1

---
+++ merge-to-json.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+input_file=$1
+
+cat "$input_file" | while read line; do
+  user=$(echo $line | sed 's#https://github.com/\(.*\)/\(.*\)#\1#')
+  repo=$(echo $line | sed 's#https://github.com/\(.*\)/\(.*\)#\2#') | sed 's/\.git$//'`
+  echo "{\"src\":\"$user\",\"repo\":\"$repo\"}"
+done | \
+jq -s add | \
+jq -c 'reduce .[] as $i ({}; .[$i.src]= ($i + .[$i.src] |= {"repo": $i.repo})) | to_entries[] | {src: .key, repo: .value.repo}' > ~/merge.json
