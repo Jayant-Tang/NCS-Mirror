aa0ea6e4804cc1bf03e72830e99df9a0332c554a
Jayant.Tang <jayant97@foxmail.com>
2023-02-10 01:22:02 +0800

    aaa7

--- merge-to-json.sh
+++ merge-to-json.sh
@@ -5,4 +5,4 @@ input_file=$1
 cat "$input_file" | while read line; do
   repo=$(echo $line | sed 's#^https://.\+\.com/\(.*\)/\(.*\)#\2#' | sed 's#[.]git$##')
   echo "{\"url\":\"$line\",\"repo\":\"$repo\"}"
-done > merge.json
+done | jq -s > ~/merge.json
