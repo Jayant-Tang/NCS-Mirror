73f010ba06fb84710b0347f88cea708e75f10c9b
Jayant.Tang <jayant.tang@nordicsemi.no>
2023-02-11 00:12:48 +0800

    new13

--- get-all-repos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -v
-
-if [ ! -d .git ]; then
-  echo "Error: Not a Git repository."
-  exit 1
-fi
-git remote -v 
-git remote -v | awk '{print $2}' | uniq >> ~/repo-list-raw.txt
-git submodule foreach --recursive "git remote -v | awk '{print \$2}' | uniq >> ~/repo-list-raw.txt"
