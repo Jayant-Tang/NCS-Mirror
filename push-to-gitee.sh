1b0a6e258b0c73c6df2dce6fea59fc73f256eea1
Jayant.Tang <jayant.tang@nordicsemi.no>
2023-02-11 14:50:46 +0800

    w1

--- push-to-gitee.sh
+++ push-to-gitee.sh
@@ -2,18 +2,20 @@
 version=$1
 gitee_user=$2
 
-echo $1
-echo $2
-
-
-
 name=$(git remote -v | awk '{print $2}' | grep -E '^https' | uniq | sed -e "s#^https://.\+\.com/\(.*\)/\(.*\)#\2#")
-echo $name
+
+echo "repo-name:{$name}"
+
 git remote add gitee git@gitee.com:$gitee_user/$name
 git remote -v
-git push gitee --all
+
+#git push gitee --all
+
+git push gitee HEAD:refs/heads/NCS-$version
 git push gitee --tags
-# git push gitee HEAD:refs/tags/NCS-$version
+
+
+
 # git checkout `git rev-list --max-parents=0 HEAD | tail -n 1`
 # git checkout -b init
 # git push gitee HEAD:refs/heads/init
